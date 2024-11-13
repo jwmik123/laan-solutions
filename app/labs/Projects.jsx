53e03db63202b783c1b11008bc7ba3726ffd4d77
Joël Mik <contact@joelmik.nl>
2024-11-13 12:15:14 +0100

    fix: grid size projects page

--- app/labs/Projects.jsx
+++ app/labs/Projects.jsx
@@ -2,10 +2,10 @@ import ProjectThumbnail from "./ProjectThumbnail";
 
 const Projects = ({ projects }) => {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-12">
       {projects.map((project, index) => (
         <div
-          className={`mb-4 md:mb-8 lg:mb-16 ${index % 4 === 2 || index % 4 === 3 ? "lg:col-span-2" : ""}`}
+          className={`mb-4 md:mb-8 lg:mb-16 ${index % 4 === 3 || index % 4 === 3 ? "lg:col-span-8" : "col-span-4"}`}
           key={project._id}
         >
           <ProjectThumbnail project={project} />
