2b6308f86e087886a95194a01f8882950d8128b7
Joël Mik <contact@joelmik.nl>
2024-11-16 10:00:57 +0100

    fix: laps and lads

--- app/lads/page.js
+++ app/lads/page.js
@@ -4,12 +4,12 @@ export default function LadsPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="mb-4 text-4xl">LAdS</h1>
-      <p className="mb-8 text-lg">
-        Welcome to Laan Drawing Solutions. We can support you with the
-        digitization of archives or existing objects.
+      <p className="mb-8 text-lg text-center text-black">
+        Welkom bij Laan Drawing Solutions. We kunnen je ondersteunen bij de
+        digitisering van archieven of bestaande objecten.
       </p>
       <Link href="/" className="text-primary-500 hover:underline">
-        Go back to Home
+        Terug naar Home
       </Link>
     </div>
   );
--- app/laps/page.js
+++ app/laps/page.js
@@ -4,12 +4,12 @@ export default function LapsPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="mb-4 text-4xl">LApS</h1>
-      <p className="mb-8 text-lg">
-        Welcome to Laan Permit Solutions. We provide thorough advice and
-        execution for your permit needs.
+      <p className="mb-8 text-lg text-center text-black">
+        Welkom bij Laan Permit Solutions. We geven uitgebreide adviezen en
+        uitvoering voor je vergunningen met gebruik van AI.
       </p>
       <Link href="/" className="text-primary-500 hover:underline">
-        Go back to Home
+        Terug naar Home
       </Link>
     </div>
   );
